297c09361b895fdedfb7cec7243d126e68af81dd
InKyuInst <148037709+InKyuInst@users.noreply.github.com>
2024-10-08 11:49:44 +0900

    qr maker

--- src/main/java/com/kosta/repository/UserAuthRepository.java
+++ src/main/java/com/kosta/repository/UserAuthRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.kosta.domain.AuthEnum;
-import com.kosta.domain.OAuthUserInfo;
 import com.kosta.entity.UserAuth;
 
 @Repository
